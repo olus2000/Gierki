81b5fe8ec31ce5bda1f73c3044bf9c811f97354c
olus2000 <alsabak@gmail.com>
2018-03-06 10:32:50 +0100

    Dokończyłem accountManager.php

--- accountManager.php
+++ accountManager.php
@@ -11,9 +11,51 @@ include "checkLogin.php";
 
     <title>Account</title>
 
-    <link rel=stylesheet href=
+    <link rel=stylesheet href=../stylesheet.css>
 
   </head>
+
+  <body>
+
+    <div class=header>
+      <a href=./><img src=images/Gierki.png></a>
+    </div>
+
+    <div class=menu>
+      <ul>
+        <li><a href=tabliczkaMnożenia.php>Tabliczka mnożenia</a></li>
+        <li><a href=formulażMnożenia.php>Formulaż tabliczki mnożenia</a></li>
+        <li><a href=licznik.php>Licznik odwiedzin</a></li>
+        <li><a href=accountManager.php>Ustawienia konta</a></li>
+        <li><a href=logout.php>Logout</a></li>
+        <li><a href=../>Back</a></li>
+      </ul>
+    </div>
+
+    <div class=main>
+
+      <h1>Użytkownik: <?php echo($_SESSION['Username']); ?>.</h1>
+
+      <?php
+
+        $stmt = $pdo->prepare('SELECT * FROM users WHERE username = :username');
+        $stmt->execute(['username' => $_SESSION['Username']]);
+        $userdata = $stmt->fetch();
+        
+        for ($userdata as $key => $val) {
+          if ($key == 'password') {
+            echo('password: <a href=signup.php>Change password</a><br>');
+          } elseif (in_array($key, ['userID', 'password', 'email'])) {
+            echo($key.': '.$val)
+          }
+        }
+
+      ?>
+
+    </div>
+
+
+  </body>
 
 </html>
 <?php include "end.php"?>
